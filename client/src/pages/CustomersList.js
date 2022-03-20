8df35a9b74c9823f8a0e0c680c6b9075600e0a84
Petec0x0 <udehonyedikachi01@gmail.com>
2022-03-20 13:35:03 +0100

    Code refactor, seperated client side from server side

--- client/src/pages/CustomersList.js
+++ client/src/pages/CustomersList.js
@@ -7,7 +7,7 @@ const CustomersList = () => {
     return (
         <>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 className="h2">Customers List</h1>
+                <h1 className="h2">Customers</h1>
             </div>
 
             <div className="row">
