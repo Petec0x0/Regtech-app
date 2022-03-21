b631048c889924495ef5ac164e09c0b6168021f9
Petec0x0 <udehonyedikachi01@gmail.com>
2022-03-21 05:38:11 +0100

    Updated 'Profile' page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,101 +3,44 @@ import { useParams } from 'react-router-dom';
 import 'styles/profile.css';
 
 const Profile = () => {
-    const {customersId} = useParams();
+    const { customersId } = useParams();
     console.log(customersId);
     return (
-        <div className="container main-secction">
+        <div className="container">
             <div className="row">
                 <div className="col-md-12 col-sm-12 col-xs-12 image-section">
-                    <img src={require('images/profilebg.jpg')} alt="Profile background" />
+                    <img src={require('images/profile_bg.jpg')} alt="Profile background" />
                 </div>
                 <div className="row user-left-part">
                     <div className="col-md-3 col-sm-3 col-xs-12 user-profil-part pull-left">
                         <div className="row ">
                             <div className="col-md-12 col-md-12-sm-12 col-xs-12 user-image text-center">
-                                <img src="https://www.jamf.com/jamf-nation/img/default-avatars/generic-user-purple.png" alt="Profile Pic" className="rounded-circle" />
-                            </div>
-                            <div className="col-md-12 col-sm-12 col-xs-12 user-detail-section1 text-center">
-                                <button className="btn btn-success btn-block follow">Onboard</button>
-                                <button className="btn btn-danger btn-block">Reject</button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="col-md-9 col-sm-9 col-xs-12 pull-right profile-right-section">
-                        <div className="row profile-right-section-row">
-                            <div className="col-md-12 profile-header">
-                                <div className="row">
-                                    <div className="col-md-8 col-sm-6 col-xs-6 profile-header-section1 pull-left">
-                                        <h1 className="pb-1">Cynthia Naza</h1>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="col-md-12">
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <ul className="nav nav-tabs" role="tablist">
-                                            <li className="nav-item">
-                                                <a className="nav-link active" href="#profile" role="tab" data-toggle="tab"><i className="fas fa-user-circle"></i> Professional profile</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" href="#buzz" role="tab" data-toggle="tab"><i className="fas fa-info-circle"></i> Detailed information</a>
-                                            </li>
-                                        </ul>
-
-                                        <div className="tab-content">
-                                            <div role="tabpanel" className="tab-pane fade show active" id="profile">
-                                                <div className="row">
-                                                    <div className="col-md-2">
-                                                        <label><strong>Name</strong></label>
-                                                    </div>
-                                                    <div className="col-md-6">
-                                                        <p>Cynthia Naza</p>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-md-2">
-                                                        <label><strong>Email</strong></label>
-                                                    </div>
-                                                    <div className="col-md-6">
-                                                        <p>juanp@gmail.com</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div role="tabpanel" className="tab-pane fade" id="buzz">
-                                                <div className="row">
-                                                    <div className="col-md-6">
-                                                        <label><strong>Date of birth</strong></label>
-                                                    </div>
-                                                    <div className="col-md-6">
-                                                        <p>03 Jan 2000</p>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-md-6">
-                                                        <label><strong>Country of residence</strong></label>
-                                                    </div>
-                                                    <div className="col-md-6">
-                                                        <p>Peru</p>
-                                                    </div>
-                                                </div>
-                                                {/* <div className="row">
-                                                    <div className="col-md-12">
-                                                        <label>Bio</label>
-                                                        <br />
-                                                        <p>Detail description</p>
-                                                    </div>
-                                                </div> */}
-                                            </div>
-
-                                        </div>
-
-                                    </div>
-                                </div>
+                                <img src={require('images/avatar_profile.png')} alt="Profile Pic" className="rounded-circle" />
                             </div>
                         </div>
                     </div>
+                </div>
+                {/* <div className="col-md-12 col-sm-12 col-xs-12 user-detail-section1 text-center">
+                    <button className="btn btn-success btn-block follow">Onboard</button>
+                    <button className="btn btn-danger btn-block">Reject</button>
+                </div> */}
+                <div className="col-md-12 col-sm-12 col-xs-12 container mt-4">
+                    <table className="table table-striped">
+                        <tbody>
+                            <tr>
+                                <td><strong>Name</strong></td>
+                                <td>Doe</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Date of birth</strong></td>
+                                <td>03 Jan 2000</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Country of residence</strong></td>
+                                <td>Peru</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div >
