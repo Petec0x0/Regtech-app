c60439d55f888d202409f2f626c5ab5483d31624
Petec0x0 <udehonyedikachi01@gmail.com>
2022-03-27 00:42:05 +0100

    Increase file upload limit size v2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
 require('dotenv').config();
